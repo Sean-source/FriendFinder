dfd6ec191130a224e2be6192aa0ecbcabcdf7b31
Sean Baklini <sbaklini@unm.edu>
2020-04-26 09:14:11 -0600

    Initial set up of get requests

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+
+const path = require("path");
+
+
+module.exports = function (app) {
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    app.get("/home", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+    // Catch all route
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+}
--- app/server.js
+++ app/server.js
@@ -1,2 +1,15 @@
-let express = require('express');
-let path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require("./routing/htmlRoutes")(app);
+require("./routing/apiRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
